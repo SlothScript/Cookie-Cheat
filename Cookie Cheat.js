9f1fa753b55906c529fc060afe2b65913a3b29be
SlothScript <106759481+SlothScript@users.noreply.github.com>
2022-07-26 12:29:50 -0500

    Create Cookie Cheat.js

---
+++ Cookie Cheat.js
@@ -0,0 +1,15 @@
+var CheatyThing = window.prompt("What thing do you want to do?\n[A] Auto click n times\b[B] Add n cookies\n[C] Add all of everything");
+if CheatyThing == "A" {
+  var n = window.prompt("How many times should it autoclick?")
+  while(i != n) {
+    Game.ClickCookie();
+    i = i + 1;
+  }
+} else if CheatyThing == "B" {
+  var n = window.prompt("How many cookies?");
+  Game.cookies = Game.cookies + n;
+} else if CheatyThing == "C" {
+  Game.cookies = 26723462839475619873;
+  Game.heavenlyChips = 35235234523453;
+  Game.lumps = 2545243529438523452345;
+}
